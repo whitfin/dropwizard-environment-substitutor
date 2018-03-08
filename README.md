b192b5f929110698cf0f571747d7c8433697d713
Isaac Whitfield <iw@whitfin.io>
2018-03-08 00:56:33 -0800

    Initial migration to open project

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # dropwizard-environment-substitutor
-Global environment overrides for Dropwizard configuration
+
+This module provides a basic way to allow overrides of all configuration at startup via environment variables. It's useful if you ship to multiple environments using things like Docker and might need to change on a whim.
+
+Although Dropwizard has built-in support for environment substitution, you would have to flag every field with the corresponding environment value, and a default when appropriate. This bundle treats the base configuration as a default, and allows you to override any property as needed.
+
+### Installation
+
+This library is available on Central, and so can be used via Maven and/or Gradle pretty easily. Just add the dependency as normal (you might have to check for the latest version, rather than what's shown below):
+
+```xml
+<dependency>
+    <groupId>io.whitfin</groupId>
+    <artifactId>dropwizard-environment-substitutor</artifactId>
+    <version>1.0.0</version>
+</dependency>
+```
+
+### Usage
+
+Simply attach the bundle to your Dropwizard application in the bootstrap phase:
+
+```java
+import io.dropwizard.Application;
+import io.dropwizard.configuration.ConfigurationSourceProvider;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class MyApplication extends Application<MyConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new MyApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<MyConfiguration> bootstrap) {
+        // setup the application configuration whilst allowing for environment overrides
+        ConfigurationSourceProvider provider = bootstrap.getConfigurationSourceProvider();
+        bootstrap.setConfigurationSourceProvider(new EnvironmentSubstitutor("MY_APP", provider));
+    }
+
+    @Override
+    public void run(MyConfiguration configuration, Environment environment) throws Exception { }
+}
+```
+
+This dictates that any property in the environment starting with "MY_APP" will be eligible for override. Consider this configuration:
+
+
+```yml
+config:
+    object:
+        key: "value"
+```
+
+If you read this in, `my_config.my_object.key == "value"`; however you can set the environment variable `MY_APP_CONFIG_OBJECT_KEY` to change it. For example `MY_APP_CONFIG_OBJECT_KEY=test` would make the value equal to `"test"`. This pattern is followed throughout; upper case the field in the configuration file and swap out any `.` for `_`, and you use a simple integer to array position.
+
+For any other functionality, please see the documentation or the code itself.
---
+++ src/main/java/io/whitfin/dropwizard/configuration/EnvironmentSubstitutor.java
@@ -0,0 +1,173 @@
+package io.whitfin.dropwizard.configuration;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import io.dropwizard.configuration.ConfigurationSourceProvider;
+import io.dropwizard.jackson.Jackson;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+
+import static com.google.common.base.CaseFormat.LOWER_UNDERSCORE;
+import static com.google.common.base.CaseFormat.UPPER_CAMEL;
+
+
+/**
+ * A delegating {@link ConfigurationSourceProvider} which replaces variables
+ * in the underlying configuration source according to any provided environment
+ * overrides (automatically).
+ */
+public class EnvironmentSubstitutor implements ConfigurationSourceProvider {
+
+    /**
+     * The original configuration provider (from the config file).
+     */
+    private final ConfigurationSourceProvider delegate;
+
+    /**
+     * A YAML mapper used to read in the base YAML configuration.
+     */
+    private final ObjectMapper mapper;
+
+    /**
+     * The namespace prefix to use to detect envrionment variables.
+     */
+    private final String namespace;
+
+    /**
+     * Create a new instance.
+     *
+     * @param delegate
+     *      the underlying {@link ConfigurationSourceProvider}.
+     */
+    public EnvironmentSubstitutor(String namespace, ConfigurationSourceProvider delegate) {
+        this.delegate = Objects.requireNonNull(delegate);
+        this.namespace = Objects.requireNonNull(namespace).toUpperCase();
+        this.mapper = Jackson.newObjectMapper(new YAMLFactory());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public InputStream open(String path) throws IOException {
+        try (final InputStream in = delegate.open(path)) {
+            final ObjectNode config = mapper.readValue(in, ObjectNode.class);
+            replace(config, this.namespace);
+            return new ByteArrayInputStream(mapper.writeValueAsBytes(config));
+        }
+    }
+
+    /**
+     * Replaces all overridden properties from the environment.
+     *
+     * @param node
+     *      the current map node to replace within.
+     * @param prefix
+     *      the prefix of the path taken so far.
+     */
+    private void replace(final ObjectNode node, String prefix) {
+        for (Iterator<Map.Entry<String, JsonNode>> it = node.fields(); it.hasNext();) {
+            Map.Entry<String, JsonNode> field = it.next();
+
+            final String key = field.getKey();
+            String path = createPrefix(key, prefix);
+
+            replace(field.getValue(), path, new Overrider() {
+                @Override
+                public void accept(String override) {
+                    node.put(key, override);
+                }
+            });
+        }
+    }
+
+    /**
+     * Replaces all overridden properties from the environment.
+     *
+     * @param node
+     *      the current list node to replace within.
+     * @param prefix
+     *      the prefix of the path taken so far.
+     */
+    private void replace(final ArrayNode node, String prefix) {
+        for (int i = 0, j = node.size(); i < j; i++) {
+            final int k = i;
+            String path = createPrefix("" + i, prefix);
+            replace(node.path(i), path, new Overrider() {
+                @Override
+                public void accept(String override) {
+                    node.set(k, TextNode.valueOf(override));
+                }
+            });
+        }
+    }
+
+    /**
+     * Replaces all overridden properties from the environment.
+     *
+     * @param node
+     *      the current node being processed.
+     * @param path
+     *      the path to the current node.
+     * @param overrider
+     *      an override to apply on value nodes.
+     */
+    private void replace(JsonNode node, String path, Overrider overrider) {
+        if (node.isObject()) {
+            replace((ObjectNode) node, path);
+            return;
+        }
+
+        if (node.isArray()) {
+            replace((ArrayNode) node, path);
+            return;
+        }
+
+        String override = System.getenv(path);
+        if (override == null) {
+            return;
+        }
+
+        overrider.accept(override);
+    }
+
+    /**
+     * Generates a path from a key and prefix.
+     *
+     * @param key
+     *      the key being processed.
+     * @param prefix
+     *      the prefix to apply to the key.
+     * @return
+     *      a String prefix to continue using.
+     */
+    private String createPrefix(String key, String prefix) {
+        return prefix + "_" + UPPER_CAMEL
+                .to(LOWER_UNDERSCORE, key)
+                .replace('.', '_')
+                .toUpperCase();
+    }
+
+    /**
+     * Small consumer interface for JDK7.
+     */
+    private interface Overrider {
+
+        /**
+         * Accepts an override as a String.
+         *
+         * @param override
+         *      the overridden value to use.
+         */
+        void accept(String override);
+    }
+}
